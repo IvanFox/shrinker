961299578d4793cc618f68cc3e5560ab37a0b9f0
ivan.lissitsnoi <ivan.lissitsnoi@transferwise.com>
2018-07-24 19:49:43 +0300

    added api controller test

---
+++ src/test/java/me/ivanlis/shrinker/controllers/ApiControllerTest.java
@@ -0,0 +1,77 @@
+package me.ivanlis.shrinker.controllers;
+
+import me.ivanlis.shrinker.api.ApiLinkModel;
+import me.ivanlis.shrinker.services.Orchestrator;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class ApiControllerTest {
+
+    WebTestClient webTestClient;
+
+
+    @Mock
+    Orchestrator orchestrator;
+
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+
+        webTestClient = WebTestClient.bindToController(new ApiController(orchestrator)).configureClient().build();
+    }
+
+    @Test
+    public void findLink() {
+        String endpoint = "find/?shortLink=";
+        String shortenUrl = "u8s1";
+
+        Mockito.when(orchestrator.findByShortUrl(shortenUrl)).thenReturn(Mono.empty());
+
+        webTestClient.get().uri(endpoint + shortenUrl)
+            .exchange()
+            .expectStatus().isOk()
+            .expectBody(ApiLinkModel.class);
+    }
+
+    @Test
+    public void findAll() {
+        String endpoint = "find/all";
+
+        Mockito.when(orchestrator.findAll()).thenReturn(Flux.empty());
+
+        webTestClient.get().uri(endpoint)
+            .exchange()
+            .expectStatus().isOk()
+            .expectBodyList(ApiLinkModel.class);
+    }
+
+    @Test
+    public void createLink() {
+        String url = "ivanfox.com";
+        Mockito.when(orchestrator.createLink(url)).thenReturn(ApiLinkModel.of("sh", url));
+
+
+        webTestClient.post().uri("/create")
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .body(Mono.just(url), String.class)
+            .exchange()
+            .expectStatus().isOk()
+            .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+            .expectBody();
+    }
+}
